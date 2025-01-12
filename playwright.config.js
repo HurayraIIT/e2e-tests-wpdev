2f6f49a324a0582fe477690caa00f43dc25d7eee
Abu Hurayra <hurayraiit@gmail.com>
2025-01-12 11:53:00 +0600

    initial commit

---
+++ playwright.config.js
@@ -0,0 +1,38 @@
+// @ts-check
+import { defineConfig, devices } from '@playwright/test';
+import { config } from 'dotenv';
+
+config();
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  retries: process.env.CI ? 1 : 0,
+  workers: process.env.CI ? 4 : 4,
+  timeout: 30 * 1000,
+
+  expect: {
+    timeout: 5_000,
+    toMatchSnapshot: { maxDiffPixelRatio: 0.03 },
+    toHaveScreenshot: { maxDiffPixelRatio: 0.03 },
+  },
+  reporter: 'html',
+  use: {
+    baseURL: process.env.BASE_URL,
+    testIdAttribute: "data-id",
+
+    screenshot: "on",
+    trace: "retain-on-failure",
+    video: "retain-on-failure",
+
+    ignoreHTTPSErrors: true,
+  },
+
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ]
+});
+
